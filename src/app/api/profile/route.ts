b6e30f3f1b78291daae636336587f98a38d74344
rkmonarch <rahulkulkarni5234@gmail.com>
2024-07-01 13:54:28 +0530

    feat: added user route

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,19 +1,40 @@
+import { ACTIONS_CORS_HEADERS, ActionGetResponse } from "@solana/actions";
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
-interface Nft {
-  nft_id: string;
-  collection: {
-    floor_prices: {
-      value: number;
-      payment_token: {
-        symbol: string;
-      };
-    }[];
-  };
+export const GET = async (req: Request) => {
+  const url = new URL(req.url!);
+  const userParams = url.searchParams.get("user");
+
+  const errorPayload: ActionGetResponse = {
+      icon: 'https://t3.ftcdn.net/jpg/01/01/89/46/360_F_101894688_RVSZUtDfPR6Cr5eBDQI7Qo5pZ01jmyK3.jpg',
+      label: `Wallet or username Not Found`,
+      description: `Not a valid user`,
+      title: `Try again with a valid wallet address`,
+  }
+
+  console.log("User Params:", userParams);
+  if (userParams) {
+     
+      const user = userParams;
+
+
+      const payload: ActionGetResponse = {
+          icon: "user.avatar_url",
+          label: `Donate 0.1 SOL to ${user}`,
+          description: `${user} has 10 contribution on github in last week`,
+          title: `send 0.1 SOL to ${user}`,
+      }
+
+      return Response.json(payload, {
+          headers: ACTIONS_CORS_HEADERS
+      })
+  } else {
+    
+      return Response.json(errorPayload, {
+          headers: ACTIONS_CORS_HEADERS
+      })
+  }
 }
 
-interface NftResponse {
-  nfts: Nft[];
-}
-
+export const OPTIONS = GET;
