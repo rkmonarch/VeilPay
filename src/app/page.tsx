c43babd421064a25b0652d6e2e5b5ec459aa67bc
Harsh <harshgajjar145@gmail.com>
2024-08-02 16:36:19 +0530

    fea: images in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import OrbitingCircles from "@/components/magicui/orbiting-circles";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -9,20 +10,30 @@ export default function Home() {
         </span>
       </div>
       <OrbitingCircles
-        className="size-[30px] border-none bg-transparent"
-        duration={20}
+        className="size-12 border-none bg-transparent"
+        duration={40}
         delay={10}
         radius={300}
       >
-        <Icons.notion />
+        <img
+          src={
+            "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png"
+          }
+          className="aspect-square rounded-full"
+        />
       </OrbitingCircles>
       <OrbitingCircles
-        className="size-[30px] border-none bg-transparent"
-        duration={20}
-        delay={10}
+        className="size-12 border-none bg-transparent"
+        duration={40}
+        delay={30}
         radius={300}
       >
-        <Icons.notion />
+        <img
+          src={
+            "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png"
+          }
+          className="aspect-square rounded-full"
+        />
       </OrbitingCircles>
       <OrbitingCircles
         className="size-[50px] border-none bg-transparent"
@@ -30,7 +41,12 @@ export default function Home() {
         duration={20}
         reverse
       >
-        <Icons.googleDrive />
+        <img
+          src={
+            "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB/logo.svg"
+          }
+          className="aspect-square rounded-full"
+        />
       </OrbitingCircles>
       <OrbitingCircles
         className="size-[50px] border-none bg-transparent"
@@ -39,7 +55,10 @@ export default function Home() {
         delay={20}
         reverse
       >
-        <Icons.gitHub />
+        <img
+          src={"https://static.jup.ag/jup/icon.png"}
+          className="aspect-square rounded-full"
+        />
       </OrbitingCircles>
       <OrbitingCircles
         className="size-[50px] border-none bg-transparent"
@@ -48,7 +67,12 @@ export default function Home() {
         delay={20}
         reverse
       >
-        <Icons.gitHub />
+        <img
+          src={
+            "https://arweave.net/hQiPZOsRZXGXBJd_82PhVdlM_hACsT_q6wqwf5cSY7I?ext=png"
+          }
+          className="aspect-square rounded-full"
+        />
       </OrbitingCircles>
       <OrbitingCircles
         className="size-[50px] border-none bg-transparent"
@@ -57,7 +81,12 @@ export default function Home() {
         delay={20}
         reverse
       >
-        <Icons.gitHub />
+        <img
+          src={
+            "https://bafkreidvkvuzyslw5jh5z242lgzwzhbi2kxxnpkic5wsvyno5ikvpr7reu.ipfs.nftstorage.link"
+          }
+          className="aspect-square rounded-full"
+        />
       </OrbitingCircles>
     </div>
   );
